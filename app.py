e5a85ac847eaa6900f572b229284cbc43fc5d6fa
GamBit1986 <mazepa86@gmail.com>
2023-03-23 17:51:07 +0300

    lesson_1 homework

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 from flask import Flask
+from flask import request
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return "Hello web!"
+
+@app.route("/user/")
+def read_user():
+    name = request.args.get("name")
+    surname = request.args.get("surname")
+    return f"User {name or '[no name]'} {surname or '[no surname]'}"
